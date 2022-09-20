3bf96b6b83c9c2dc63bbd0a6143eecdac5675c5f
ebasson <esteban.basson@pwa-assurance.fr>
2022-09-20 16:55:56 +0200

    commit mardi 20/09/2022

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+/*début lecture directives html*/
+Eba_Directive = function(cb) {
+	let z, i, elmnt, file, xhttp;
+	z = document.getElementsByTagName("*");
+	for (i = 0; i < z.length; i++) {
+		elmnt = z[i];
+		file = elmnt.getAttribute("eba-directive");
+		if (file) {
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (this.readyState === 4) {
+					this.status === 200  ? elmnt.innerHTML = this.responseText : console.log('tamer');
+					if (this.status === 404) {elmnt.innerHTML = "Page not found.";}
+					elmnt.removeAttribute("eba-directive");
+					Eba_Directive(cb);
+				}
+			}
+			xhttp.open("GET", file, true);
+			xhttp.send();
+			return;
+		}
+	}
+	if (cb) cb();
+};
+
+/*début lecture directives html*/
+
+
+/*debut conditionnement affichage des pages*/
+function eba_visibilite(thingId){
+	let targetElement;
+	let elements;
+	targetElement = document.getElementById(thingId) ;
+	elements = document.getElementsByClassName("eba-visibilite-Element")
+	
+	for (let i = 0; i < elements.length; i++) {
+		
+		if( !targetElement.isSameNode(elements[i]) ){
+			elements[i].style.display = "none" ;
+		}
+		
+	}
+	
+	if (targetElement.style.display === "none") {
+		targetElement.style.display = "" ;
+	} else{
+		targetElement.style.display = "none" ;
+	}
+}
+/*fin conditionnement affichage des pages*/
